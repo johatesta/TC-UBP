a19ba1858d9eed6a2726779a6048448aff12ebdf
johatesta <johatesta@gmail.com>
2021-12-03 17:57:03 -0300

    symbol table done

--- TP-FINAL/proyectofinal/src/main/java/app/tabladesimbolos/MyToken.java
+++ TP-FINAL/proyectofinal/src/main/java/app/tabladesimbolos/MyToken.java
@@ -1,6 +1,19 @@
-public class MyToken {
-    
-}
-public class id {
-    
+package tabladesimbolos;
+
+import org.antlr.runtime.CommonToken;
+
+
+public class MyToken extends CommonToken {
+    private static final long serialVersionUID = 1L;
+    public String srcName;
+
+    public MyToken(int type, String text) {
+        super(type, text);
+    }
+
+    @Override
+    public String toString() {
+        String t = super.toString();
+        return srcName + ":" + t;
+    }
 }
--- TP-FINAL/proyectofinal/src/main/java/app/tabladesimbolos/variable.java
+++ TP-FINAL/proyectofinal/src/main/java/app/tabladesimbolos/variable.java
@@ -1,3 +1,24 @@
-public class variable {
+package tabladesimbolos;
+
+public class Variable extends ID {
+
+    public Variable() {}
+
+    public Variable(String nombre, String tipo) {
+        super(nombre, tipo);
+    }
+
+    @Override
+    public String toString() {
+        String variable = this.getTipo() + " " + this.getNombre();
+
+        if (this.isUsado())
+            variable += " [Variable usada]";
+        else
+            variable += " [Variable sin usar]";
+            
+        return variable;
+    }
     
 }
+
